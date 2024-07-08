c68f17192c646b158d4e68dcb78374dfe75a5b0c
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-07-09 03:28:07 +0330

    Add home page image elements

--- src/main/java/app/game/gui/HomeController.java
+++ src/main/java/app/game/gui/HomeController.java
@@ -1,11 +1,23 @@
 package app.game.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class HomeController {
 
     @FXML
+    private Text txt_diamonds;
+
+    @FXML
+    private Text txt_stars;
+
+    @FXML
     private void initialize() {
         System.out.println("HELLO PAGE");
+    }
+
+    @FXML
+    private void handleSettingButton() {
+        System.out.println("Setting page button!");
     }
 }
