692cbdd38cd024d5440f8aae15f78d7c614503dc
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-07-08 08:04:04 +0330

    Add Tower model

--- src/main/java/app/game/model/Tower.java
+++ src/main/java/app/game/model/Tower.java
@@ -1,5 +1,30 @@
 package app.game.model;
 
-public class Tower {
+public abstract class Tower {
 
+    private int destruction;
+    private int construction;
+
+    Tower(int destruction, int construction) {
+        this.destruction = destruction;
+        this.construction = construction;
+    }
+
+    // GETTERS
+    public int getDestruction() {
+        return destruction;
+    }
+
+    public int getConstruction() {
+        return construction;
+    }
+
+    // SETTERS
+    public void setDestruction(int destruction) {
+        this.destruction = destruction;
+    }
+
+    public void setConstruction(int construction) {
+        this.construction = construction;
+    }
 }
